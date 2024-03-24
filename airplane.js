eedead63cff13014a21a93964011966bc71987fb
Rata13NS <161605200+Rata13NS@users.noreply.github.com>
2024-03-24 13:05:19 +0200

    Update airplane.js

--- airplane.js
+++ airplane.js
@@ -3,6 +3,8 @@ let ctx = canvas.getContext('2d');
 let min = 1500;
 let max = 500;
 let crashCounter = 0;
+let gameOver = false;
+let seconds = 0;
 
 // Dimensiunea panzei
 canvas.width = window.innerWidth;
@@ -24,50 +26,60 @@ airplane.draw()
 
 // Mut avionul
 document.addEventListener('keydown', function(event) {
-    if (event.key === 'ArrowLeft' && airplane.x > 200) {
-        ctx.clearRect(airplane.x, airplane.y, airplane.width, airplane.height);
-        airplane.x -= 80;
-    } else if (event.key === 'ArrowRight' && airplane.x < 1700) {
-        ctx.clearRect(airplane.x, airplane.y, airplane.width, airplane.height);
-        airplane.x += 80;
+    if (gameOver === false) {
+        if (event.key === 'ArrowLeft' && airplane.x > 200) {
+            ctx.clearRect(airplane.x, airplane.y, airplane.width, airplane.height);
+            airplane.x -= 80;
+        } else if (event.key === 'ArrowRight' && airplane.x < 1700) {
+            ctx.clearRect(airplane.x, airplane.y, airplane.width, airplane.height);
+            airplane.x += 80;
+        }
+        airplane.draw();
     }
-    airplane.draw();
 });
 
 setInterval(showObstacles, 2000);
+
 // Creez obstacole si le fac sa se miste
 function showObstacles() {
-    let obstacle = {
-        w: Math.floor(Math.random() * (max - min + 1) + min),
-        z: 0,
-        width: 100,
-        height: 100,
-        color: 'white',
-        speed: 5,
-        draw: function() {
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.w, this.z, this.width, this.height);
-        },
-        update: function() {
-            this.z += this.speed;
-        }
-    }
-
-    function animate() {
-        requestAnimationFrame(animate);
-        ctx.clearRect(obstacle.w, obstacle.z, obstacle.width, obstacle.height);
-        obstacle.update();
-        obstacle.draw();
-        if (airplane.x < obstacle.w + obstacle.width &&
-            airplane.x + airplane.width > obstacle.w &&
-            airplane.y < obstacle.z + obstacle.height &&
-            airplane.y + airplane.height > obstacle.z) {
-            ++crashCounter;
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            if (crashCounter === 1) {
-                alert('Game OVER! You hit an obstacle!');
+    if (gameOver === false) {
+        let obstacle = {
+            w: Math.floor(Math.random() * (max - min + 1) + min),
+            z: 0,
+            width: 100,
+            height: 100,
+            color: 'white',
+            speed: 5,
+            draw: function() {
+                ctx.fillStyle = this.color;
+                ctx.fillRect(this.w, this.z, this.width, this.height);
+            },
+            update: function() {
+                this.z += this.speed;
             }
         }
+    
+        function animate() {
+            requestAnimationFrame(animate);
+            ctx.clearRect(obstacle.w, obstacle.z, obstacle.width, obstacle.height);
+            obstacle.update();
+            obstacle.draw();
+            if (airplane.x < obstacle.w + obstacle.width &&
+                airplane.x + airplane.width > obstacle.w &&
+                airplane.y < obstacle.z + obstacle.height &&
+                airplane.y + airplane.height > obstacle.z) {
+                ++crashCounter;
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                if (crashCounter === 1) {
+                    gameOver = true;
+                    alert('Game OVER! You hit an obstacle! You have ' + seconds + ' points! REFRESH THE PAGE TO START A NEW GAME.');
+                }
+            }
+        }
+        animate();
     }
-    animate();
+}
+
+function incrementSeconds() {
+    seconds += 1;
 }
