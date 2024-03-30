6a2e9a38a1330d0d953a08f30469a04bc9968213
Rata13NS <161605200+Rata13NS@users.noreply.github.com>
2024-03-30 12:32:12 +0200

    Update airplaneBONUSS.js

--- airplaneBONUSS.js
+++ airplaneBONUSS.js
@@ -4,11 +4,13 @@ let min = 700;
 let max = 1300;
 let crashCounter = 0;
 let gameOver = false;
+let destroyedObstacle = false;
 let seconds = 0;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let obstacles = [];
 let ammunitions = [];
+let destroyedObstacles = 0;
 
 // Create the airplane
 let airplane = {
@@ -40,31 +42,46 @@ document.addEventListener('keydown', function(event) {
 
 // Create the obstacles, make them move and display the points
 function showObstacles() {
-    let destroyedObstacle = false;
     if (gameOver === false) {
+        destroyedObstacle = false;
         let obstacle = {
             w: Math.floor(Math.random() * (max - min + 1) + min),
             z: 0,
             width: 100,
             height: 100,
             color: 'white',
-            speed: 3,
+            speed: 6,
             draw: function() {
                 ctx.fillStyle = this.color;
                 ctx.fillRect(this.w, this.z, this.width, this.height);
             },
             update: function() {
-                if (destroyedObstacle === false) {
-                    this.z += this.speed;
-                }
+                this.z += this.speed;
             }
         }
+        obstacles.push(obstacle); 
     
         function animateObstacles() {
-            requestAnimationFrame(animateObstacles);
-            ctx.clearRect(obstacle.w, obstacle.z, obstacle.width, obstacle.height);
-            obstacle.update();
-            obstacle.draw();
+            for (let i = 0; i < ammunitions.length; ++i) {
+                for (let j = 0; j < obstacles.length; ++j) {
+                    if (ammunitions[i].m < obstacles[j].w + obstacles[j].width &&
+                        ammunitions[i].m + ammunitions[i].width > obstacles[j].w &&
+                        ammunitions[i].n < obstacles[j].z + obstacles[j].height &&
+                        ammunitions[i].n + ammunitions[i].height > obstacles[j].z) {
+                            ctx.clearRect(ammunitions[i].m, ammunitions[i].n, ammunitions[i].width, ammunitions[i].height + 2);
+                            ctx.clearRect(obstacles[j].w, obstacles[j].z, obstacles[j].width, obstacles[j].height);
+                            destroyedObstacle = true;
+                            ++destroyedObstacles;
+                            console.log(destroyedObstacle);
+                    }  
+                }
+            }
+            if (destroyedObstacle === false) {
+                requestAnimationFrame(animateObstacles);
+                ctx.clearRect(obstacle.w, obstacle.z, obstacle.width, obstacle.height);
+                obstacle.update();
+                obstacle.draw();
+            }
             if (airplane.x < obstacle.w + obstacle.width &&
                 airplane.x + airplane.width > obstacle.w &&
                 airplane.y < obstacle.z + obstacle.height &&
@@ -73,50 +90,41 @@ function showObstacles() {
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
                 if (crashCounter === 1) {
                     gameOver = true;
-                    alert('Game OVER! You hit an obstacle! You have ' + seconds + ' points! REFRESH THE PAGE TO START A NEW GAME.');
-                }
+                    alert('Game OVER! You hit an obstacle! You have ' + destroyedObstacles + ' points! REFRESH THE PAGE TO START A NEW GAME.');
+                } 
             }
         }
         animateObstacles();
+    }
+}  
 
-        document.addEventListener('keydown', function(event) {
-            if (gameOver === false) {
-                if (event.code === 'Space') {
-                    let ammunition = {
-                        m: airplane.x + 5,
-                        n: airplane.y - 10,
-                        width: 8,
-                        height: 8,
-                        color: 'red',
-                        speed: 2,
-                        draw: function() {
-                            ctx.fillStyle = this.color;
-                             ctx.fillRect(this.m, this.n, this.width, this.height);
-                        },
-                        update: function() {
-                            if (destroyedObstacle === false) {
-                                this.n -= this.speed;
-                            }
-                        }
-                    }
-                    
-                    function animateAmmunition() {
-                        requestAnimationFrame(animateAmmunition);
-                        ctx.clearRect(ammunition.m, ammunition.n, ammunition.width, ammunition.height);
-                        ammunition.update();
-                        ammunition.draw();
-                        if (ammunition.m < obstacle.w + obstacle.width &&
-                            ammunition.m + ammunition.width > obstacle.w &&
-                            ammunition.n < obstacle.z + obstacle.height &&
-                            ammunition.n + ammunition.height > obstacle.z) {
-                                ctx.clearRect(ammunition.m, ammunition.n, ammunition.width, ammunition.height + 2);
-                                ctx.clearRect(obstacle.w, obstacle.z, obstacle.width, obstacle.height);
-                                destroyedObstacle = true;
-                        }
-                    }
-                    animateAmmunition();
+document.addEventListener('keydown', function(event) {
+    if (gameOver === false) {
+        if (event.code === 'Space') {
+            let ammunition = {
+                m: airplane.x + 5,
+                n: airplane.y - 10,
+                width: 8,
+                height: 8,
+                color: 'red',
+                speed: 4,
+                draw: function() {
+                    ctx.fillStyle = this.color;
+                    ctx.fillRect(this.m, this.n, this.width, this.height);
+                },
+                update: function() {
+                    this.n -= this.speed;
                 }
             }
-        });
+            ammunitions.push(ammunition);
+
+            function animateAmmunition() {
+                requestAnimationFrame(animateAmmunition);
+                ctx.clearRect(ammunition.m, ammunition.n, ammunition.width, ammunition.height + 2);
+                ammunition.update();
+                ammunition.draw();
+            }
+            animateAmmunition();
+        }
     }
-}
+});
